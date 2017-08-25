4418cf11d560ec830243aca6d434e8b55299b08d
HanMengcheng <feifanhanmc@163.com>
2017-08-25 13:36:40 +0800

    修改README.md

--- mytest.py
+++ mytest.py
@@ -8,26 +8,9 @@ class MyWXBot(WXBot):
         if not msg['msg_type_id'] in (0, 1, 99):    #过滤掉无意义的消息
             print json.JSONEncoder().encode(msg)
             ES().save_data(msg=msg)
-#             if msg['content']['type'] == 3:     #图片消息 
-#                 self.get_msg_img(msg['msg_id'])
-#             if msg['msg_type_id'] == 2 :    #文件消息
-#                 pass
-#             elif msg['msg_type_id'] == 3 :  #群消息
-#                 if msg['content']['type'] == 0:   #文本
-#                     pass
-#             elif msg['msg_type_id'] == 4 :  #联系人消息
-#                 if msg['content']['type'] == 0:   #文本
-#                     pass
-#             elif msg['msg_type_id'] == 5 :  #公众号消息
-#                 pass
-#             elif msg['msg_type_id'] == 6 :  #特殊账号消息
-#                 pass
-        
-
 
 
 def main():
-    #bot
     bot = MyWXBot()
     bot.DEBUG = True
     bot.conf['qr'] = 'png'
